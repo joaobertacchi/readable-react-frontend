c4a72576389232421690e0a0842e15ae69f36948
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2019-05-04 13:44:52 -0300

    feat: add comments reducer

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,18 +1,30 @@
 // @flow
 
-import { RECEIVE_COMMENTS } from '../actions/comments';
-import { type ReceiveCommentsActionType } from '../actions/comments';
+import {
+  type CommentActionType,
+  RECEIVE_COMMENTS,
+  ADD_COMMENT,
+  DELETE_COMMENT,
+  VOTE_COMMENT,
+  UPDATE_COMMENT,
+} from '../actions/comments';
 import { type CommentsStateType } from '../types/state';
 import { type CommentType } from '../types/comment';
 
-export default function comments (state: CommentsStateType = {}, action: ReceiveCommentsActionType): CommentsStateType {
+const comments = (state: CommentsStateType = {}, action: CommentActionType): CommentsStateType => {
   switch(action.type) {
     case RECEIVE_COMMENTS:
       return {
         ...state,
         ...action.comments.reduce((acc: CommentsStateType, comment: CommentType): CommentsStateType => ({ ...acc, [comment.id]: comment}), {}),
       };
+    case ADD_COMMENT:
+    case DELETE_COMMENT:
+    case VOTE_COMMENT:
+    case UPDATE_COMMENT:
     default:
       return state;
   }
-}
+};
+
+export default comments; 
