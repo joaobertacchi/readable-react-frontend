523b3393ecfd5d198045c33e57ec046d15a09d7d
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2019-05-04 20:05:52 -0300

    feat: add post actions

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,15 +1,58 @@
 // @flow
 
-import { type PostType } from '../types/post';
+import { type PostType, type PostId } from '../types/post';
+import {
+  upVotePost,
+  downVotePost,
+  addPost,
+  deletePost,
+  updatePost,
+} from '../utils/api';
 
 export const RECEIVE_POSTS = 'RECEIVE_POSTS';
 export const ADD_POST = 'ADD_POST';
 export const DELETE_POST = 'DELETE_POST';
+export const UPDATE_POST = 'UPDATE_POST';
+export const VOTE_POST = 'VOTE_POST';
 
-export type ReceivePostsActionType = {
+export type VoteOption = 'up' | 'down';
+
+export type Vote = {
+  option: VoteOption,
+  postId: PostId,
+};
+
+type VotePostActionType = {
+  type: 'VOTE_POST',
+  vote: Vote,
+};
+
+type ReceivePostsActionType = {
   posts: Array<PostType>,
   type: 'RECEIVE_POSTS',
 };
+
+type AddPostActionType = {
+  post: PostType,
+  type: 'ADD_POST',
+};
+
+type DeletePostActionType = {
+  postId: PostId,
+  type: 'DELETE_POST',
+};
+
+type UpdatePostActionType = {
+  post: PostType,
+  type: 'UPDATE_POST',
+}
+
+export type PostActionType =
+  AddPostActionType |
+  DeletePostActionType |
+  UpdatePostActionType |
+  ReceivePostsActionType |
+  VotePostActionType;
 
 export function receivePosts(posts: Array<PostType>): ReceivePostsActionType {
   return {
@@ -17,3 +60,59 @@ export function receivePosts(posts: Array<PostType>): ReceivePostsActionType {
     posts,
   };
 }
+
+export function votePost(vote: Vote): VotePostActionType {
+  return {
+    type: VOTE_POST,
+    vote,
+  };
+}
+
+export const addPostAction = (post: PostType): AddPostActionType => ({
+  type: ADD_POST,
+  post,
+});
+
+export const deletePostAction = (postId: PostId): DeletePostActionType => ({
+  type: DELETE_POST,
+  postId,
+});
+
+export const updatePostAction = (post: PostType): UpdatePostActionType => ({
+  type: UPDATE_POST,
+  post,
+});
+
+export function handleVotePost(vote: Vote): Function {
+  return (dispatch: Function): ?Promise<void> => {
+    if (vote.option === 'up')
+      return upVotePost(vote.postId)
+        .then((): void => dispatch(votePost(vote)));
+    else if (vote.option === 'down')
+      return downVotePost(vote.postId)
+        .then((): void => dispatch(votePost(vote)));
+    else
+      return;
+  };
+}
+
+export const handleAddPost = (
+  post: PostType,
+): Function => (dispatch: Function): ?Promise<void> => {
+  return addPost(post)
+    .then((result: PostType): void => dispatch(addPostAction(result)));
+};
+
+export const handleUpdatePost = (
+  post: PostType,
+): Function => (dispatch: Function): ?Promise<void> => {
+  return updatePost(post.id, post)
+    .then((result: PostType): void => dispatch(updatePostAction(result)));
+};
+
+export const handleDeletePost = (
+  postId: PostId,
+): Function => (dispatch: Function): ?Promise<void> => {
+  return deletePost(postId)
+    .then((post: PostType): void => dispatch(deletePostAction(post.id)));
+};
