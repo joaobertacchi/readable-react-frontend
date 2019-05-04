3121465361b3a47b7363ad85f1819a7495f6e5f5
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2019-05-04 13:16:09 -0300

    feat: add comment actions

--- src/actions/comments.js
+++ src/actions/comments.js
@@ -1,28 +1,127 @@
 // @flow
-import { getComments } from '../utils/api';
+import * as API from '../utils/api';
 
 import { type CommentType } from '../types/comment';
 import { type PostId } from '../types/post';
+import { type CommentId } from '../types/comment';
 
 export const RECEIVE_COMMENTS = 'RECEIVE_COMMENTS';
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const DELETE_COMMENT = 'DELETE_COMMENT';
+export const UPDATE_COMMENT = 'UPDATE_COMMENT';
+export const VOTE_COMMENT = 'VOTE_COMMENT';
 
-export type ReceiveCommentsActionType = {
+export type VoteOption = 'up' | 'down';
+
+export type Vote = {
+  commentId: CommentId,
+  option: VoteOption,
+};
+
+type VoteCommentActionType = {
+  type: 'VOTE_COMMENT',
+  vote: Vote,
+};
+
+type ReceiveCommentsActionType = {
   comments: Array<CommentType>,
   type: 'RECEIVE_COMMENTS',
 };
 
-export function receiveComments(comments: Array<CommentType>): ReceiveCommentsActionType {
+type AddCommentActionType = {
+  comment: CommentType,
+  type: 'ADD_COMMENT',
+};
+
+type DeleteCommentActionType = {
+  commentId: CommentId,
+  type: 'DELETE_COMMENT',
+};
+
+type UpdateCommentActionType = {
+  comment: CommentType,
+  type: 'UPDATE_COMMENT',
+}
+
+export type CommentActionType =
+  AddCommentActionType |
+  DeleteCommentActionType |
+  UpdateCommentActionType |
+  ReceiveCommentsActionType |
+  VoteCommentActionType;
+
+export const receiveCommentsAction = (comments: Array<CommentType>): ReceiveCommentsActionType => {
   return {
     type: RECEIVE_COMMENTS,
     comments,
   };
-}
+};
 
-export function handleInitialComments(postId: PostId): Function {
-  return (dispatch: Function): Promise<void> => {
-    return getComments(postId)
-      .then((comments: Array<CommentType>) => {
-        dispatch(receiveComments(comments));
-      });
+export function voteComment(vote: Vote): VoteCommentActionType {
+  return {
+    type: VOTE_COMMENT,
+    vote,
   };
 }
+
+export function handleVoteComment(vote: Vote): Function {
+  return (dispatch: Function): ?Promise<void> => {
+    if (vote.option === 'up')
+      return API.upVoteComment(vote.commentId)
+        .then((): void => dispatch(voteComment(vote)));
+    else if (vote.option === 'down')
+      return API.downVoteComment(vote.commentId)
+        .then((): void => dispatch(voteComment(vote)));
+    else
+      return;
+  };
+}
+
+export const addCommentAction = (comment: CommentType): AddCommentActionType => ({
+  type: ADD_COMMENT,
+  comment,
+});
+
+export const deleteCommentAction = (commentId: CommentId): DeleteCommentActionType => {
+  return {
+    type: DELETE_COMMENT,
+    commentId,
+  };
+};
+
+export const updateCommentAction = (comment: CommentType): UpdateCommentActionType => ({
+  type: UPDATE_COMMENT,
+  comment,
+});
+
+export const handleAddComment = (
+  comment: CommentType,
+): Function => (dispatch: Function): ?Promise<void> => {
+  return API.addComment(comment)
+    .then((result: CommentType): void => dispatch(addCommentAction(result)));
+};
+
+export const handleUpdateComment = (
+  comment: CommentType,
+): Function => (dispatch: Function): ?Promise<void> => {
+  return API.updateComment(comment.id, comment)
+    .then((result: CommentType): void => dispatch(updateCommentAction(result)));
+};
+
+export const handleDeleteComment = (commentId: CommentId): Function => {
+  return (dispatch: Function): Promise<void> => {
+    return API.deleteComment(commentId)
+      .then(({ id }: CommentType) => {
+        dispatch(deleteCommentAction(id));
+      });
+  };
+};
+
+export const handleInitialComments = (postId: PostId): Function => {
+  return (dispatch: Function): Promise<void> => {
+    return API.getComments(postId)
+      .then((comments: Array<CommentType>) => {
+        dispatch(receiveCommentsAction(comments));
+      });
+  };
+};
