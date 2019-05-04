4b575d7cebc3a71f77829ee0c95ba787f8f4f351
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2019-05-04 13:48:10 -0300

    fixup! feat: add global types

--- src/types/post.js
+++ src/types/post.js
@@ -4,6 +4,16 @@ import { type IdType } from './shared';
 
 export type PostId = IdType;
 
+export const Constants = {
+  sortType: {
+    DATE: 'timestamp',
+    SCORE: 'voteScore',
+    TITLE: 'title',
+  },
+};
+
+export type SortType = 'timestamp' | 'voteScore' | 'title';
+
 export type PostType = {
   author: string,
   body: string,
